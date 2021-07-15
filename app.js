2d9d1867f7a38b8e6f26191dc2181e1968278ed3
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2021-07-15 22:15:39 +0530

    close

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/personDB",{useNewUrlParser:true ,useUnifiedTopology: true});
 
-// //create the schema
-// const personSchema = new mongoose.Schema({
-//     name:String,
-//     age : Number,
-//     address : String
-// });
+//create the schema
+const personSchema = new mongoose.Schema({
+    name:String,
+    age : Number,
+    address : String
+});
 
-// //create model
-// const Person = mongoose.model("Person",personSchema);
+//create model
+const Person = mongoose.model("Person",personSchema);
 
-// //create a person object
-// const person = new Person({
-//     name:"dinuka",
-//     age:22,
-//     address: "Lion house"
-// });
+//create a person object
+const person = new Person({
+    name:"dinuka",
+    age:22,
+    address: "Lion house"
+});
 
-// //save person to the database
-// person.save();
+//save person to the database
+person.save();
 
 //creating the schema
 const fruitSchema = new mongoose.Schema({
@@ -30,29 +30,29 @@ const fruitSchema = new mongoose.Schema({
 //creating the model
 const Fruit = mongoose.model("Fruit",fruitSchema);
 
-// //creating objects
-// const mango = new Fruit({
-//     name:"mango",
-//     rating:10
-// });
+//creating objects
+const mango = new Fruit({
+    name:"mango",
+    rating:10
+});
 
-// const apple = new Fruit({
-//     name: "apple",
-//     rating: 6
-// });
+const apple = new Fruit({
+    name: "apple",
+    rating: 6
+});
 
-// const orange = new Fruit({
-//     name:"orange",
-//     rating:9
-// });
+const orange = new Fruit({
+    name:"orange",
+    rating:9
+});
 
-// Fruit.insertMany([mango,apple,orange],(error,result)=>{
-//     if(error){
-//         console.log(error);
-//     }else{
-//         console.log("Successfull!");
-//     }
-// });
+Fruit.insertMany([mango,apple,orange],(error,result)=>{
+    if(error){
+        console.log(error);
+    }else{
+        console.log("Successfull!");
+    }
+});
 
 /*All callbacks in Mongoose use the pattern callback(error, result).
  If an error occurs executing the query, the error parameter will contain an error document and result will be null. 
