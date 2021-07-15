47c029a44e4a510a971d460da1b1927ecce512e8
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2021-07-15 21:37:51 +0530

    creating the model

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/personDB",{useNewUrlParser:true ,useUnifiedTopology: true});
 
-//create the schema
-const personSchema = new mongoose.Schema({
+// //create the schema
+// const personSchema = new mongoose.Schema({
+//     name:String,
+//     age : Number,
+//     address : String
+// });
+
+// //create model
+// const Person = mongoose.model("Person",personSchema);
+
+// //create a person object
+// const person = new Person({
+//     name:"dinuka",
+//     age:22,
+//     address: "Lion house"
+// });
+
+// //save person to the database
+// person.save();
+
+//creating the schema
+const fruitSchema = new mongoose.Schema({
     name:String,
-    age : Number,
-    address : String
+    rating:Number
 });
 
-//create model
-const Person = mongoose.model("Person",personSchema);
-
-//create a person object
-const person = new Person({
-    name:"dinuka",
-    age:22,
-    address: "Lion house"
-});
-
-//save person to the database
-person.save();
+//creating the model
+const Fruit = mongoose.model("Fruit",fruitSchema);
