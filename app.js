813bdb12b94768e786b2f62c8da8a929217e4d95
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2021-07-19 21:27:34 +0530

    add student model

--- app.js
+++ app.js
@@ -3,6 +3,52 @@ mongoose.connect("mongodb://localhost:27017/studentDB",{useNewUrlParser:true ,us
 
 //create a schema
 
+const studentSchema =  new mongoose.Schema({
+    name: {
+        type: String,
+        required:[true,"name should be specified!"]
+    },
+
+    age:{
+        type: String,
+        min:10
+
+    }
+});
+
+//create a model
+
+const Student = new mongoose.model("Student",studentSchema);
+
+//------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//create a schema
+
 const studentSchema = new mongoose.Schema({
     name:{
         type: String,
