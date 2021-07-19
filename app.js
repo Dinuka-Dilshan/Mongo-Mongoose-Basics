9fb791ce32494bfbb47e53ee5713b343cc487613
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2021-07-19 21:54:54 +0530

    relations

--- app.js
+++ app.js
@@ -1,29 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/fruitDB",{useNewUrlParser:true ,useUnifiedTopology: true});
 
-//create a schema
-
-const studentSchema =  new mongoose.Schema({
-    name: {
-        type: String,
-        required:[true,"name should be specified!"]
-    },
-
-    age:{
-        type: String,
-        min:10
-
-    },
-
-    favouriteFruit: fruitSchema
-});
-
-//create a model
-
-const Student = new mongoose.model("Student",studentSchema);
-
-//------------------------------------------------------------------------------------
-
 
 //create a schema
 
@@ -46,17 +23,59 @@ const fruitSchema = new mongoose.Schema({
 const Fruit = new mongoose.model("Fruit",fruitSchema);
 
 
+//------------------------------------------------------------------------------------
+
+//create a schema
+
+const studentSchema =  new mongoose.Schema({
+    name: {
+        type: String,
+        required:[true,"name should be specified!"]
+    },
+
+    age:{
+        type: String,
+        min:10
+
+    },
+
+    favouriteFruit: fruitSchema
+});
+
+//create a model
+
+const Student = new mongoose.model("Student",studentSchema);
+
+
+
+
+
 
 // create a fruit object
 
 const fruit = new Fruit({
-    name:"apple",
+    name:"mango",
     rating:8
 })
 
-fruit.save();
+fruit.save((error,result)=>{
+    if(error){
+        console.log(error);
+    }else{
+        mongoose.connection.close();
+    }
+});
 
+//create a student object who likes mango
 
+const student = new Student({
+    name:"dinuka",
+    age:22,
+    favouriteFruit:fruit
+
+})
+
+student.save();
 
 
 
