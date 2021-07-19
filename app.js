d4c0f9e0c78a85d6161c3a3438fcc3fb173dae11
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2021-07-19 21:41:33 +0530

    d

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/studentDB",{useNewUrlParser:true ,useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/fruitDB",{useNewUrlParser:true ,useUnifiedTopology: true});
 
 //create a schema
 
@@ -13,7 +13,9 @@ const studentSchema =  new mongoose.Schema({
         type: String,
         min:10
 
-    }
+    },
+
+    favouriteFruit: fruitSchema
 });
 
 //create a model
@@ -45,6 +47,14 @@ const Fruit = new mongoose.model("Fruit",fruitSchema);
 
 
 
+// create a fruit object
+
+const fruit = new Fruit({
+    name:"apple",
+    rating:8
+})
+
+fruit.save();
 
 
 
@@ -66,54 +76,3 @@ const Fruit = new mongoose.model("Fruit",fruitSchema);
 
 
 
-
-//create a schema
-
-const studentSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        maxLength:10,
-        required:[true,"name is not specified!"]
-    },
-
-    age:{
-        type: Number,
-        min:18,
-        max:60
-    }
-});
-
-//create the model
-const Student = new mongoose.model("student",studentSchema);
-
-//create a student object
-
-const student = new Student({
-    name:"dilshan",
-    age:20
-});
-
-//close the connection if saved successfully
-student.save((error,result)=>{
-    if(result){
-        mongoose.connection.close();
-    }
-});
-
-//update details
-Student.updateOne({ name:"dilshan"},{name:"kamal"},(error,result)=>{
-    if(error){
-        console.log(error);
-    }else{
-        console.log("Successfull!");
-    };
-});
-
-Student.deleteOne({name:"kamal"},(error,result)=>{
-    if(error){
-        console.log(error)
-    }else{
-        mongoose.connection.close();
-        console.log("deleted!");
-    }
-});
